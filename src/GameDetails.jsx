39c88d20c971948bcea7f90586733933c71ee9da
Pablo Arvizu <parvizu@gmail.com>
2018-01-15 22:59:01 -0800

    UI updates for Game Details. Added new team color header

--- src/GameDetails.jsx
+++ src/GameDetails.jsx
@@ -9,6 +9,7 @@ export default class GameDetails extends Component {
 
 		this.handleSelectTeamPlayer = this.handleSelectTeamPlayer.bind(this);
 		this.getTeamPlayers = this.getTeamPlayers.bind(this);
+		this.getScoringBreakdown = this.getScoringBreakdown.bind(this);
 	}
 
 	handleSelectTeamPlayer(e) {
@@ -38,10 +39,57 @@ export default class GameDetails extends Component {
 		});
 	}
 
+	getScoringBreakdown() {
+		 let breakdown = Object.keys(this.props.scoreBreakdown).map((period) => {
+			let periodScore = this.props.scoreBreakdown[period];
+
+			return (
+				<div className="period-scoring">
+					<div className="period-scoring-section">
+						{periodScore.label}
+					</div>
+					<div className="period-scoring-home period-scoring-section">
+						{periodScore.home}
+					</div>
+					<div className="period-scoring-away period-scoring-section">
+						{periodScore.away}
+					</div>
+				</div>
+			);
+		});
+
+		breakdown.unshift(<div className="period-scoring">
+					<div className="period-scoring-section">
+						&nbsp;
+					</div>
+					<div className="period-scoring-home period-scoring-section">
+						{this.props.homeTeam}
+					</div>
+					<div className="period-scoring-away period-scoring-section">
+						{this.props.awayTeam}
+					</div>
+				</div>);
+
+		return breakdown;
+	}
+
 	render() { 
 		return (
 			<div className="game-menu-container">
-				<div className="team-details">
+				<div className="teams-header">
+					<h2 className={"team-name team-name-away "+this.props.awayTeam}>
+						<div className="team-name-text">{this.props.awayRoster.teamCity + " " + this.props.awayRoster.teamName}</div>
+						<div className={"team-name-logo "+ this.props.awayTeam}></div>
+						<div className="team-score team-away-score">{this.props.scoreBreakdown.final.away}</div>
+					</h2>
+					<h2 className={"team-name team-name-home "+this.props.homeTeam}>
+						<div className={"team-name-logo "+this.props.homeTeam}></div>
+						<div className="team-name-text">{this.props.homeRoster.teamCity + " " + this.props.homeRoster.teamName}
+						</div>
+						<div className="team-score team-home-score">{this.props.scoreBreakdown.final.home}</div>
+					</h2>
+				</div>
+				<div className="team-details team-away">
 					<div className={'game-details-team-logo ' + this.props.awayTeam}>
 						<div className="team-players-list">
 							{this.getTeamPlayers(this.props.awayRoster.players,this.props.awayTeam, this.props.awayPlayersSelected)}
@@ -49,10 +97,11 @@ export default class GameDetails extends Component {
 					</div>
 				</div>
 				<div className="game-details">
-					<h3>{this.props.awayTeam}</h3> @ 
-					<h3>{this.props.homeTeam}</h3>
+					<div className="period-scoring-container">
+						{this.getScoringBreakdown()}
+					</div>
 				</div>
-				<div className="team-details">
+				<div className="team-details team-home">
 					<div className={'game-details-team-logo ' + this.props.homeTeam}>
 						<div className="team-players-list">
 							{this.getTeamPlayers(this.props.homeRoster.players,this.props.homeTeam, this.props.homePlayersSelected)}
